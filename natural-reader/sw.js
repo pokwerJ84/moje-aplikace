a6f2dbb5fbc5f564c207d8706469cfdfe9467ba2
pokwerJ84 <pokwer1984@gmail.com>
2026-09-13 14:40:48 +0900

    Add Natural Reader service worker

---
+++ natural-reader/sw.js
@@ -0,0 +1,34 @@
+const CACHE = "natural-reader-v1";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
