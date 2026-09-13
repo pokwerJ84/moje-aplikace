06e12e33ec00f53d14bbe11ad956e3e73103e301
pokwerJ84 <pokwer1984@gmail.com>
2026-09-13 22:13:10 +0900

    Release Poky Reader v10

--- natural-reader/sw.js
+++ natural-reader/sw.js
@@ -1,11 +1,11 @@
-const VERSION = "v9";
+const VERSION = "v10";
 const CACHE = `poky-reader-${VERSION}`;
 const CORE = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./cloud-tts-v8.js",
+  "./cloud-tts-v10.js",
   "./updater.js",
   "./manifest.webmanifest",
   "./icon-poky-180.png"
